49bd4034d8d310583f681cfa219e0aa30b279c4d
Taremeh <tarekalakmeh@hotmail.ch>
2017-02-11 19:45:47 +0100

    Notification Plugin Logic implemented

--- apphovenAlpha/app/pages/home/home.component.ts
+++ apphovenAlpha/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { Page } from "ui/page";
 import { Router } from "@angular/router";
 import { AndroidApplication, AndroidActivityBackPressedEventData } from "application";
 import * as application from "application";
+import * as LocalNotifications from "nativescript-local-notifications";
 
 
 @Component({
@@ -32,6 +33,28 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     navigateTo(page: string){
         this._router.navigate([page]);
+    }
+
+    notify(){
+        LocalNotifications.schedule([{
+            id: 1,
+            title: 'The title',
+            body: 'And here the body',
+            at: new Date(new Date().getTime() + (5 * 1000)) // 5 seconds from now
+        }]).then(
+            function() {
+                console.log("Notification scheduled");
+            },
+            function(error) {
+                console.log("scheduling error: " + error);
+            }
+        );
+
+        LocalNotifications.getScheduledIds().then(
+            function(ids) {
+                console.log("ID's: " + ids);
+            }
+        );
     }
 
 }
